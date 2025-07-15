c88c0d5063b225b3fd725cc47f02027328466ce3
Vincenza <vinni2690@hotmail.com>
2025-07-15 18:40:44 +0200

    dropdown defeated me

--- src/components/Booking.tsx
+++ src/components/Booking.tsx
@@ -1,6 +1,8 @@
 import axios from "axios"
 import { useState } from "react"
 import { useParams } from "react-router-dom"
+import { Dropdown } from "./Dropdown"
+
 
 interface NewBookingObject {
   checkInDate: string,
@@ -74,8 +76,69 @@ const Booking = function () {
 
     return(
         
-        <section>
-            {/* reservations */}
+        <section className="flex flex-col bg-white-300 dark:bg-green-950 py-50 items-center justify-content text-black dark:text-gray-200 p-6 gap-8">
+
+            {/* tasti accomadion type + number of guests */}
+            <div>
+                <button id="dropdownDefaultButton" data-dropdown-toggle="dropdown" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" type="button">Dropdown button <svg className="w-2.5 h-2.5 ms-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
+                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 4 4 4-4"/>
+                </svg>
+                </button>
+                <div id="dropdown" className="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow-sm w-44 dark:bg-gray-700">
+                    <ul className="py-2 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="dropdownDefaultButton">
+                    <li>
+                        <a href="#" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Dashboard</a>
+                    </li>
+                    <li>
+                        <a href="#" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Settings</a>
+                    </li>
+                    <li>
+                        <a href="#" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Earnings</a>
+                    </li>
+                    <li>
+                        <a href="#" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Sign out</a>
+                    </li>
+                    </ul>
+                </div>
+                <div className="grid md:grid-cols-2 md:gap-6 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                    <select name="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" value={newBooking.accommodationType} onChange={(e) => {
+                        
+                        setNewBooking({
+                        ...newBooking,
+                        accommodationType: e.target.value,
+                        })
+                    }}>
+                        <option value="PLOT">Plot</option>
+                        <option value="MOBILEHOME">Mobile Home</option>
+                        <option value="GLAMPING">Glamping</option>
+                    </select>
+
+                </div>
+                
+                    <select className="bg-gray-300 text-black" name="accommodation" id="accommodation">
+                        <option className="bg-white" value="PLOT">Plot</option>
+                        <option className="bg-white" value="MOBILEHOME">Mobile Home</option>
+                        <option className="bg-white" value="GLAMPING">Glamping</option>
+                    </select> 
+                    
+                    
+                    
+                                 
+                
+            </div>
+
+
+            {/* calendario */}
+            <div></div>
+
+            {/* available accommodations? */}
+            <div></div>
+
+
+
+
+
+            {/* reservation form*/}
             <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto lg:py-0">
             <form className="max-w-md mx-auto" onSubmit={(e)=>{
                             e.preventDefault()
