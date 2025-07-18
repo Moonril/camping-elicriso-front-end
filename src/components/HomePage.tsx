8294df48d22887e41d6cfd30aa1d874256f3ea5b
Vincenza <vinni2690@hotmail.com>
2025-07-18 11:20:47 +0200

    homepage design improved

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -6,18 +6,19 @@ const HomePage = function (){
         <section className="min-h-screen bg-slate-50 dark:bg-black dark:text-white ">
             {/* hero */}
             <div id="hero" className="flex flex-col justify-center py-80 2xl:py-80 px-5 items-center gap-8 h-screen bg-[url(https://images.pexels.com/photos/388303/pexels-photo-388303.jpeg)] bg-cover">
-                <h3 id="hero-welcome" className="text-2xl md:text-3xl delius-regular xl:text-5xl">Welcome to</h3>
-                <h1 className="text-4xl md:text-6xl xl:text-8xl delius-regular">Camping Elicrisi</h1>
+                <h3 id="hero-welcome" className="text-2xl md:text-3xl delius-regular xl:text-5xl text-white text-shadow-md/80">Welcome to</h3>
+                <h1 className="text-4xl md:text-6xl xl:text-8xl delius-regular text-white0 text-shadow-md/80">Camping Elicrisi</h1>
             </div>
             {/* rest of the page */}
-            <div className="relative text-black z-10 bg-white">
-                <img src="https://cdn.prod.website-files.com/5c0db0b58da8f065edfa6d46/5c0dbc7736137d1606dd867d_painted%20edge.png" className="absolute top-[-20px] lg:top-[-30px] xl:top-[-50px] left-0 z-[-10]" alt="paint-banner" />
+            <div className="relative text-black z-10 bg-orange-50">
+                {/* white painted: https://cdn.prod.website-files.com/5c0db0b58da8f065edfa6d46/5c0dbc7736137d1606dd867d_painted%20edge.png */}
+                <img src="painted-edge-cream.png" className="absolute top-[-20px] lg:top-[-30px] xl:top-[-50px] left-0 z-[-10]" alt="paint-banner" />
                 <div className="flex flex-col justify-center items-center gap-8 py-20 px-10 md:px-20"> {/* big container */}
                     
                     {/* about us */}
                     <div className="flex flex-col md:flex-row justify-between items-center gap-8 pb-15">
 
-                        <h2 className="text-4xl quicksand-titles font-medium md:self-start text-center flex-1">About Us</h2>
+                        <h2 className="text-4xl quicksand-titles font-medium md:self-start text-center flex-1 text-shadow-md/30">About Us</h2>
 
                         <p className=" flex-2">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sed vel recusandae corrupti natus distinctio amet velit accusamus numquam adipisci dicta, nisi voluptatibus corporis, obcaecati, explicabo officia nobis illo? Sit, dolores.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sed vel recusandae corrupti natus distinctio amet velit accusamus numquam adipisci dicta, nisi voluptatibus corporis, obcaecati, explicabo officia nobis illo? Sit, dolores.
                         Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa reiciendis vero odio ullam voluptatibus, mollitia eius fuga facere. Dolorum aut atque numquam aliquam ea error delectus fugiat corporis exercitationem magni!
@@ -29,16 +30,16 @@ const HomePage = function (){
 
                     {/* Explore */}
                     
-                        <div className="flex flex-col justify-between items-center gap-8 center px-10 pb-15">
+                        <div className="flex flex-col justify-between items-center gap-8 center lg:px-10 pb-15">
 
                         
-                            <h2 className="text-3xl quicksand-titles font-medium">Esplora &#9978;</h2>
+                            <h2 className="text-3xl quicksand-titles font-medium text-shadow-md/20">Esplora &#9978;</h2>
                             
                             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sed vel recusandae corrupti natus distinctio amet velit accusamus numquam adipisci dicta, nisi voluptatibus corporis, obcaecati, explicabo officia nobis illo? Sit, dolores.</p>
                             <div className="flex flex-col sm:flex-row gap-8">
                                 <div className="flex-1">
                                     <Link to={'/accommodations'}>
-                                        <img src="https://images.pexels.com/photos/2123285/pexels-photo-2123285.jpeg" alt="" className="aspect-3/2 object-cover object-bottom rounded-2xl" />
+                                        <img src="https://images.pexels.com/photos/2123285/pexels-photo-2123285.jpeg" alt="" className="aspect-3/2 object-cover object-bottom rounded-2xl shadow-lg/30 transition delay-150 duration-300 ease-in-out hover:-translate-y-1 hover:scale-110" />
                                     </Link>
                                     <div className="p-5">
                                         <h5 className="mb-2 text-xl font-bold tracking-tight text-gray-900 dark:text-black text-center">Le nostre piazzole</h5>
@@ -47,7 +48,7 @@ const HomePage = function (){
                                 </div>
                                 <div className="flex-1">
                                     <Link to={'/accommodations'}>
-                                        <img src="https://images.pexels.com/photos/2174992/pexels-photo-2174992.jpeg" alt="" className="aspect-3/2 object-cover object-bottom rounded-2xl" />
+                                        <img src="https://images.pexels.com/photos/2174992/pexels-photo-2174992.jpeg" alt="" className="aspect-3/2 object-cover object-bottom rounded-2xl shadow-lg/30 transition delay-150 duration-300 ease-in-out hover:-translate-y-1 hover:scale-110" />
                                     </Link>
                                     <div className="p-5">
                                         <h5 className="mb-2 text-xl font-bold tracking-tight text-gray-900 dark:text-black text-center">I nostri bungalows</h5>
@@ -56,7 +57,7 @@ const HomePage = function (){
                                 </div>
                                 <div className="flex-1">
                                     <Link to={'/accommodations'}>
-                                        <img src="https://images.pexels.com/photos/17396037/pexels-photo-17396037.jpeg" alt="" className="aspect-3/2 object-cover object-bottom rounded-2xl" />
+                                        <img src="https://images.pexels.com/photos/17396037/pexels-photo-17396037.jpeg" alt="" className="aspect-3/2 object-cover object-bottom rounded-2xl shadow-lg/30 transition delay-150 duration-300 ease-in-out hover:-translate-y-1 hover:scale-110" />
                                     </Link>
                                     <div className="p-5">
                                         <h5 className="mb-2 text-xl font-bold tracking-tight text-gray-900 dark:text-black text-center">Le nostre tende lussuose</h5>
@@ -69,18 +70,18 @@ const HomePage = function (){
 
 
                         {/* facilities */}
-                        <div className="w-full max-w-screen-xl mx-auto flex flex-col items-center justify-content gap-8 px-10">
+                        <div className="w-full max-w-screen-xl mx-auto flex flex-col items-center justify-content gap-8 pb-10">
 
                             
-                            <h3 className="text-3xl quicksand-titles font-medium">Le strutture &#128703;</h3>
-                            <p>Goditi una mattina rinfrescante in piscina, un bel pranzo nel nostro ristorante e un dopo pranzo relax nelle nostre amache</p>
+                            <h3 className="text-3xl quicksand-titles font-medium text-shadow-md/20">Le strutture</h3>
+                            <p>Goditi una mattina rinfrescante in piscina, riposati nelle nostre amache e goditi un aperitivo nel nostro ristorante</p>
 
                             <div className="w-full max-w-screen-xl mx-auto">
                                 <div className="w-full h-[200px]">
                                     <img
                                     src="https://images.pexels.com/photos/19781912/pexels-photo-19781912.jpeg"
                                     alt="swimming pool"
-                                    className="w-full h-full object-cover object-[25%_70%] rounded-2xl"
+                                    className="w-full h-full object-cover object-[25%_70%] rounded-2xl  shadow-lg/30"
                                     />
                                 </div>
                                 <div className="py-2">
@@ -94,7 +95,7 @@ const HomePage = function (){
                                     <img
                                     src="https://images.pexels.com/photos/1229753/pexels-photo-1229753.jpeg"
                                     alt="swimming pool"
-                                    className="w-full h-full object-cover object-[25%_70%] rounded-2xl"
+                                    className="w-full h-full object-cover object-[25%_70%] rounded-2xl  shadow-lg/30"
                                     />
                                 </div>
                                 <div className="py-2">
@@ -106,7 +107,7 @@ const HomePage = function (){
                                     <img
                                     src="https://images.pexels.com/photos/1237073/pexels-photo-1237073.jpeg"
                                     alt="swimming pool"
-                                    className="w-full h-full object-cover object-[25%_70%] rounded-2xl"
+                                    className="w-full h-full object-cover object-[25%_70%] rounded-2xl  shadow-lg/30"
                                     />
                                 </div>
                                 <div className="py-2">
@@ -117,36 +118,36 @@ const HomePage = function (){
 
                         {/* amenities */}
 
-                        <div className="flex flex-col justify-between items-center gap-8">
-                            <h3 className="text-3xl quicksand-titles font-medium">I servizi &#128703;</h3>
+                        <div className="flex flex-col justify-between items-center gap-8 px-10 lg:px-20 pb-10">
+                            <h3 className="text-3xl quicksand-titles font-medium text-shadow-md/20">I servizi &#128703;</h3>
 
                             <div className="flex flex-col gap-8">
                                 <div className="flex flex-col sm:flex-row gap-8">
                                     <div className="flex-1">
-                                        <img src="https://images.pexels.com/photos/17791283/pexels-photo-17791283.jpeg" alt="" className="aspect-3/2 object-cover object-bottom rounded-2xl" />
+                                        <img src="https://images.pexels.com/photos/17791283/pexels-photo-17791283.jpeg" alt="" className="aspect-3/2 object-cover object-bottom rounded-2xl shadow-lg/30" />
                                         <div className="p-5">
-                                            <h5 className="mb-2 text-xl font-bold tracking-tight text-gray-900 dark:text-black text-center">Bathrooms, hot shower</h5>
+                                            <h5 className="mb-2 text-xl font-bold tracking-tight text-gray-900 dark:text-black text-center">Bagni e docce calde</h5>
                                         </div>
                                     </div>
                                     <div className="flex-1">
-                                        <img src="https://images.pexels.com/photos/67184/pexels-photo-67184.jpeg" alt="" className="aspect-3/2 object-cover object-bottom rounded-2xl" />
+                                        <img src="https://images.pexels.com/photos/67184/pexels-photo-67184.jpeg" alt="" className="aspect-3/2 object-cover object-bottom rounded-2xl shadow-lg/30" />
                                         <div className="p-5">
-                                            <h5 className="mb-2 text-xl font-bold tracking-tight text-gray-900 dark:text-black text-center">outdoor sink</h5>
+                                            <h5 className="mb-2 text-xl font-bold tracking-tight text-gray-900 dark:text-black text-center">Lavandini all'aperto</h5>
                                         </div>
                                     </div>
                                 </div>
                                 <div className="flex flex-col sm:flex-row gap-8">
                                     
                                     <div className="flex-1">
-                                        <img src="https://images.pexels.com/photos/1309067/pexels-photo-1309067.jpeg" alt="" className="aspect-3/2 object-cover object-bottom rounded-2xl" />
+                                        <img src="https://images.pexels.com/photos/1309067/pexels-photo-1309067.jpeg" alt="" className="aspect-3/2 object-cover object-bottom rounded-2xl shadow-lg/30" />
                                         <div className="p-5">
-                                            <h5 className="mb-2 text-xl font-bold tracking-tight text-gray-900 dark:text-black text-center">grilling stations</h5>
+                                            <h5 className="mb-2 text-xl font-bold tracking-tight text-gray-900 dark:text-black text-center">Barbecue</h5>
                                         </div>
                                     </div>
                                     <div className="flex-1">
-                                        <img src="https://images.pexels.com/photos/1061640/pexels-photo-1061640.jpeg" alt="" className="aspect-3/2 object-cover object-bottom rounded-2xl" />
+                                        <img src="https://images.pexels.com/photos/1061640/pexels-photo-1061640.jpeg" alt="" className="aspect-3/2 object-cover object-bottom rounded-2xl shadow-lg/30" />
                                         <div className="p-5">
-                                            <h5 className="mb-2 text-xl font-bold tracking-tight text-gray-900 dark:text-black text-center">Bonfire pits</h5>
+                                            <h5 className="mb-2 text-xl font-bold tracking-tight text-gray-900 dark:text-black text-center">Falò</h5>
                                         </div>
                                     </div>
                                 </div>
@@ -157,24 +158,34 @@ const HomePage = function (){
 
                         {/* dintorni */}
                         <div className="flex flex-col justify-between items-center gap-8 center">
-                            <h3 className="text-3xl quicksand-titles font-medium">Dintorni</h3>
+                            <h3 className="text-3xl quicksand-titles font-medium text-shadow-md/20">Dintorni</h3>
+                            <p>Visita la toscana! da spiagge caraibiche a discoteche a picco sul mare</p>
                             <div className="flex flex-col sm:flex-row gap-8">
                                 <div className="flex-1">
-                                    <img src="https://images.pexels.com/photos/1309067/pexels-photo-1309067.jpeg" alt="" className="aspect-3/2 object-cover object-bottom rounded-2xl" />
-                                        <div className="p-5">
-                                            <h5 className="mb-2 text-xl font-bold tracking-tight text-gray-900 dark:text-black text-center">Spiagge</h5>
+                                    <Link to={'/surroundings'}>
+                                        <img src="https://images.pexels.com/photos/2049422/pexels-photo-2049422.jpeg" alt="" className="aspect-3/2 object-cover object-bottom rounded-2xl shadow-lg/30 transition delay-150 duration-300 ease-in-out hover:-translate-y-1 hover:scale-110" />
+                                    </Link>
+                                        <div className="p-2">
+                                            <h5 className="mb-2 text-xl font-bold tracking-tight text-gray-900 dark:text-black text-center">Mare</h5>
+                                            <p className="text-center">descrizione spiagge etc?</p>
                                         </div>
                                 </div>
                                 <div className="flex-1">
-                                    <img src="https://images.pexels.com/photos/1309067/pexels-photo-1309067.jpeg" alt="" className="aspect-3/2 object-cover object-bottom rounded-2xl" />
-                                        <div className="p-5">
+                                    <Link to={'/surroundings'}>
+                                        <img src="https://images.pexels.com/photos/2629233/pexels-photo-2629233.jpeg" alt="" className="aspect-3/2 object-cover object-center rounded-2xl shadow-lg/30 transition delay-150 duration-300 ease-in-out hover:-translate-y-1 hover:scale-110" />
+                                    </Link>
+                                        <div className="p-2">
                                             <h5 className="mb-2 text-xl font-bold tracking-tight text-gray-900 dark:text-black text-center">Trekking</h5>
+                                            <p className="text-center">descrizione natura etc?</p>
                                         </div>
                                 </div>
                                 <div className="flex-1">
-                                    <img src="https://images.pexels.com/photos/1309067/pexels-photo-1309067.jpeg" alt="" className="aspect-3/2 object-cover object-bottom rounded-2xl" />
-                                        <div className="p-5">
-                                            <h5 className="mb-2 text-xl font-bold tracking-tight text-gray-900 dark:text-black text-center">Nightlife</h5>
+                                    <Link to={'/surroundings'}>
+                                        <img src="https://images.pexels.com/photos/4430307/pexels-photo-4430307.jpeg" alt="" className="aspect-3/2 object-cover object-center rounded-2xl shadow-lg/30 transition delay-150 duration-300 ease-in-out hover:-translate-y-1 hover:scale-110" />
+                                    </Link>
+                                        <div className="p-2">
+                                            <h5 className="mb-2 text-xl font-bold tracking-tight text-gray-900 dark:text-black text-center">Vita notturna</h5>
+                                            <p className="text-center">descrizione discoteche ristoranti etc?</p>
                                         </div>
                                 </div>
                             </div>
@@ -188,7 +199,7 @@ const HomePage = function (){
             </div>
             {/* find us */}
                         <div className="flex flex-col bg-gray-300 items-center gap-8 p-6">
-                            <h3 className="text-3xl">Dove siamo</h3>
+                            <h3 className="text-3xl text-shadow-md/20">Dove siamo</h3>
                             
                             <div className="flex flex-col md:flex-row w-full gap-20">
                                 <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d5790.225290108554!2d10.32744894740432!3d43.47911951957426!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x12d5e61e2519c089%3A0x70bfd41bcbc381dd!2sCalafuria%20Province%20of%20Livorno%2C%20Italy!5e0!3m2!1sen!2ses!4v1752496589592!5m2!1sen!2ses" width="600" height="450" loading="lazy" className="w-full flex-2"></iframe>
--- src/components/MyNavBar.tsx
+++ src/components/MyNavBar.tsx
@@ -1,6 +1,11 @@
 import { useState } from "react";
 import { Link, useLocation } from "react-router-dom"
 import { useAuth } from "../context/AuthContext";
+import { CiHome } from "react-icons/ci";
+import { LuNotebookPen, LuTent } from "react-icons/lu";
+import { IoRestaurantOutline } from "react-icons/io5";
+import { BiWorld } from "react-icons/bi";
+import { MdPermDeviceInformation } from "react-icons/md";
 
 
 const MyNavBar = function (){
@@ -16,7 +21,7 @@ const MyNavBar = function (){
 
   console.log("Navbar render:", isLoggedIn, user);
     return(
-      <header className='relative bg-transparent px-5 py-2 absolute top-0 left-0 right-0 z-10 font-sans bg-white'>          
+      <header className='relative px-5 py-2 absolute top-0 left-0 right-0 z-101 font-sans bg-orange-50'>          
         <section className="flex items-center justify-between ">
             <h1 className="text-3xl font-medium">
               <Link to={"/"} className={location.pathname === '/accommodations' ? 'underline' : ''}><img src="tent-night-.svg" alt="" className="w-10"/> </Link>
@@ -57,29 +62,29 @@ const MyNavBar = function (){
               </button>
           </section>
 
-          <img src="https://cdn.prod.website-files.com/5c0db0b58da8f065edfa6d46/5c0dbc7736137d1606dd867d_painted%20edge.png" className="absolute inset-9 md:inset-0 w-full  z-[-10] -bottom-16 left-0 right-0" alt="paint-banner" />
+          <img src="painted-edge-cream.png" className="absolute inset-9 md:inset-0 w-full  z-[-10] -bottom-16 left-0 right-0" alt="paint-banner" />
 
           {/* dropdown menu mobile */}
           
             {isOpen && (
-              <section id="mobile-menu" className={`fixed top-0 left-0 h-screen w-full bg-green-950 opacity-90 text-5xl text-white transform transition-transform duration-300 ease-in-put z-20 ${isOpen ? 'translate-x-0' : '-translate-x-full'}`}>
+              <section id="mobile-menu" className={`fixed top-0 left-0 h-screen w-full bg-green-950 opacity-90 text-5xl text-white transform transition-transform duration-300 ease-in-put ${isOpen ? 'translate-x-0' : '-translate-x-full'}`}>
                 <button onClick={toggleDropdown} className="text-4xl self-end px-6">&times;</button>
-                  <nav className="flex flex-col items-center justify-center gap-6 h-full text-3xl" aria-label="mobile">
-                        <Link to={"/"} onClick={() => setIsOpen(false)} className={`hover:opacity-80 text-green-700 ${location.pathname === '/' ? 'underline' : ''}`}>Home</Link>
-                        <Link to={"/accommodations"} onClick={() => setIsOpen(false)} className={`hover:opacity-80 text-green-700 ${location.pathname === '/accommodations' ? 'underline' : ''}`}>Alloggi</Link>
-                        <Link to={"/bookings"} onClick={() => setIsOpen(false)} className={`hover:opacity-90 text-green-700 ${location.pathname === '/bookings' ? 'underline' : ''}`}>Prenota</Link>
-                        <Link to={"/news"} onClick={() => setIsOpen(false)} className={`hover:opacity-80 text-green-700 ${location.pathname === '/news' ? 'underline' : ''}`}>News</Link>
-                        <Link to={"/restaurant"} onClick={() => setIsOpen(false)} className={`hover:opacity-80 text-green-700 ${location.pathname === '/restaurant' ? 'underline' : ''}`}>Restaurant</Link>
-                        <Link to={"/contact"} onClick={() => setIsOpen(false)} className={`hover:opacity-80 text-green-700 ${location.pathname === '/contact' ? 'underline' : ''}`}>Info & Contatti</Link>
+                  <nav className="flex flex-col items-left justify-center gap-6 h-full text-3xl ps-15" aria-label="mobile">
+                        <Link to={"/"} onClick={() => setIsOpen(false)} className={`hover:opacity-80 flex flex-row gap-2 ${location.pathname === '/' ? 'underline text-green-100' : 'text-green-600'}`}><CiHome /> Home</Link>
+                        <Link to={"/accommodations"} onClick={() => setIsOpen(false)} className={`hover:opacity-80 flex flex-row gap-2 ${location.pathname === '/accommodations' ? 'underline text-green-100' : 'text-green-300'}`}><LuTent /> Alloggi</Link>
+                        <Link to={"/bookings"} onClick={() => setIsOpen(false)} className={`hover:opacity-90 flex flex-row gap-2 ${location.pathname === '/bookings' ? 'underline text-green-100' : 'text-green-300'}`}><LuNotebookPen /> Prenota</Link>
+                        <Link to={"/restaurant"} onClick={() => setIsOpen(false)} className={`hover:opacity-80 flex flex-row gap-2 ${location.pathname === '/news' ? 'underline text-green-100' : 'text-green-300'}`}><IoRestaurantOutline /> Ristorante</Link>
+                        <Link to={"/surroundings"} onClick={() => setIsOpen(false)} className={`hover:opacity-80 flex flex-row gap-2 ${location.pathname === '/restaurant' ? 'underline text-green-100' : 'text-green-300'}`}><BiWorld /> Dintorni</Link>
+                        <Link to={"/contact"} onClick={() => setIsOpen(false)} className={`hover:opacity-80 flex flex-row gap-2 ${location.pathname === '/contact' ? 'underline text-green-100' : 'text-green-300'}`}><MdPermDeviceInformation /> Info & Contatti</Link>
                         <br />
-                        <Link to={"/myBookings"} onClick={() => setIsOpen(false)} className={`hover:opacity-80 text-sm text-green-700 ${location.pathname === '/myBookings' ? 'underline' : ''}`}>My reservations</Link>
+                        <Link to={"/myBookings"} onClick={() => setIsOpen(false)} className={`hover:opacity-80 flex flex-row gap-2 text-sm ${location.pathname === '/myBookings' ? 'underline text-green-100' : 'text-green-300'}`}>Le mie prenotazioni</Link>
                         {isLoggedIn ? (
                           <div>
                             <span>Ciao, {user?.name}</span>
                             <button onClick={logout}>Logout</button>
                           </div>
                         ) : (
-                          <Link to={"/login"} onClick={() => setIsOpen(false)} className={`hover:opacity-80 text-sm text-green-700 ${location.pathname === '/myBookings' ? 'underline' : ''}`}>Admin</Link>
+                          <Link to={"/login"} onClick={() => setIsOpen(false)} className={`hover:opacity-80 text-sm ${location.pathname === '/myBookings' ? 'underline text-green-100' : 'text-green-300'}`}>Admin</Link>
                         )
 
                         }
