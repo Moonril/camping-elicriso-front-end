6ec47d6ca9dc6bff3328c96c345d562f421b57ea
Vincenza <vinni2690@hotmail.com>
2025-07-25 09:58:31 +0200

    fixes

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -5,7 +5,7 @@ const HomePage = function (){
 
     return(
 
-        <section className="min-h-screen bg-slate-50 dark:bg-black dark:text-white ">
+        <section className="min-h-screen bg-slate-50  text-white ">
             {/* hero */}
             <div id="hero" className="flex flex-col justify-center py-80 2xl:py-80 px-5 items-center gap-8 h-screen bg-[url(https://images.pexels.com/photos/388303/pexels-photo-388303.jpeg)] bg-cover">
                 <h3 id="hero-welcome" className="text-2xl md:text-3xl delius-regular xl:text-5xl text-white text-shadow-md/80">Welcome to</h3>
@@ -22,11 +22,15 @@ const HomePage = function (){
 
                         <h2 className="text-4xl quicksand-titles font-medium md:self-start text-center flex-1 text-shadow-md/30">About Us</h2>
 
-                        <p className=" flex-2">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sed vel recusandae corrupti natus distinctio amet velit accusamus numquam adipisci dicta, nisi voluptatibus corporis, obcaecati, explicabo officia nobis illo? Sit, dolores.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sed vel recusandae corrupti natus distinctio amet velit accusamus numquam adipisci dicta, nisi voluptatibus corporis, obcaecati, explicabo officia nobis illo? Sit, dolores.
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa reiciendis vero odio ullam voluptatibus, mollitia eius fuga facere. Dolorum aut atque numquam aliquam ea error delectus fugiat corporis exercitationem magni!
-                        Quibusdam commodi quis aperiam officiis voluptatibus error veniam fugit molestiae hic? Voluptatibus minus commodi ut et obcaecati maiores distinctio autem quidem, itaque in illo quaerat, molestias maxime a voluptates sint!
-                        Facilis error quod sequi, praesentium corrupti dicta non veritatis sed quas, repellendus laboriosam numquam enim quo possimus doloribus natus! Doloremque ex exercitationem reiciendis iusto accusamus explicabo dolorum ducimus ad obcaecati.
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sed vel recusandae corrupti natus distinctio amet velit accusamus numquam adipisci dicta, nisi voluptatibus corporis, obcaecati, explicabo officia nobis illo? Sit, dolores.</p>
+                        <p className=" flex-2 text-lg">Nato dal desiderio di vivere a contatto con la natura e accogliere viaggiatori da ogni angolo del mondo, il nostro campeggio è il frutto di un sogno condiviso da due amici di lunga data, Luca e Martina. Amanti del mare, dell’avventura e delle serate sotto le stelle, hanno trasformato la loro passione in un luogo in cui semplicità, relax e autenticità si incontrano ogni giorno.
+                            <br />
+
+                        Situato a pochi passi dal mare, il campeggio offre piazzole immerse nel verde, angoli tranquilli per leggere all’ombra degli alberi e spazi comuni dove nascono nuove amicizie. Che tu viaggi con la tua tenda, il camper o cerchi un bungalow accogliente, qui troverai sempre un sorriso e un’atmosfera familiare.
+                        <br />
+                        Qui il tempo rallenta. Le piazzole sono circondate dal verde, il profumo della salsedine accompagna le giornate, e ogni tramonto sulla spiaggia sembra dipinto apposta per i nostri ospiti. Il campeggio offre anche bungalow accoglienti, servizi curati e spazi comuni dove chiacchierare, giocare, cucinare o semplicemente lasciarsi cullare dal suono delle onde.
+                            <br />
+                        Un luogo dove si viene per fermarsi, ma si riparte sempre con un ricordo speciale nel cuore.
+                        </p>
                     </div>
 
 
@@ -37,15 +41,15 @@ const HomePage = function (){
                         
                             <h2 className="text-3xl quicksand-titles font-medium text-shadow-md/20">Esplora &#9978;</h2>
                             
-                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sed vel recusandae corrupti natus distinctio amet velit accusamus numquam adipisci dicta, nisi voluptatibus corporis, obcaecati, explicabo officia nobis illo? Sit, dolores.</p>
+                            
                             <div className="flex flex-col sm:flex-row gap-8">
                                 <div className="flex-1">
                                     <Link to={'/accommodations'}>
                                         <img src="https://images.pexels.com/photos/2123285/pexels-photo-2123285.jpeg" alt="" className="aspect-3/2 object-cover object-bottom rounded-2xl shadow-lg/30 transition delay-50 duration-300 ease-in-out hover:-translate-y-1 hover:scale-110" />
                                     </Link>
                                     <div className="p-5">
-                                        <h5 className="mb-2 text-xl font-bold tracking-tight text-gray-900 dark:text-black text-center">Le nostre piazzole</h5>
-                                        <p>piccola descrizione sulle nostre piazzole situate nel centro del bosco</p>
+                                        <h5 className="mb-2 text-xl font-bold tracking-tight text-black text-center">Le nostre piazzole</h5>
+                                        <p>Semplicità e natura allo stato puro. Le nostre piazzole sono immerse nel verde, ombreggiate e dotate di allacci elettrici, perfette per tende, camper o roulotte. A due passi dai servizi e dal mare, offrono tutto ciò che serve per vivere un soggiorno libero, rilassante e a contatto con la terra.</p>
                                     </div>
                                 </div>
                                 <div className="flex-1">
@@ -53,8 +57,8 @@ const HomePage = function (){
                                         <img src="https://images.pexels.com/photos/2174992/pexels-photo-2174992.jpeg" alt="" className="aspect-3/2 object-cover object-bottom rounded-2xl shadow-lg/30 transition delay-50 duration-300 ease-in-out hover:-translate-y-1 hover:scale-110" />
                                     </Link>
                                     <div className="p-5">
-                                        <h5 className="mb-2 text-xl font-bold tracking-tight text-gray-900 dark:text-black text-center">I nostri bungalows</h5>
-                                        <p>piccola descrizione sui nostri situate sulla collina vista mare</p>
+                                        <h5 className="mb-2 text-xl font-bold tracking-tighttext-black text-center">I nostri bungalows</h5>
+                                        <p>Per chi ama la comodità senza rinunciare all’atmosfera del campeggio. I nostri bungalow in legno, accoglienti e funzionali, sono ideali per famiglie, coppie o amici. Dotati di angolo cottura, bagno privato e veranda esterna, promettono momenti di relax assoluto con il profumo della salsedine nell’aria.</p>
                                     </div>
                                 </div>
                                 <div className="flex-1">
@@ -62,8 +66,8 @@ const HomePage = function (){
                                         <img src="https://images.pexels.com/photos/17396037/pexels-photo-17396037.jpeg" alt="" className="aspect-3/2 object-cover object-bottom rounded-2xl shadow-lg/30 transition delay-50 duration-300 ease-in-out hover:-translate-y-1 hover:scale-110" />
                                     </Link>
                                     <div className="p-5">
-                                        <h5 className="mb-2 text-xl font-bold tracking-tight text-gray-900 dark:text-black text-center">Le nostre tende lussuose</h5>
-                                        <p>piccola descrizione sulle nostre tende lussuose all'ultima moda</p>
+                                        <h5 className="mb-2 text-xl font-bold tracking-tighttext-black text-center">Le nostre tende lussuose</h5>
+                                        <p>L’anima selvaggia incontra il comfort. Il nostro glamping offre tende attrezzate dallo stile unico, con letti veri, arredi curati e scorci sulla natura circostante. Per chi cerca un’esperienza originale, romantica e indimenticabile, con quel tocco di magia sotto le stelle.</p>
                                     </div>
                                 </div>
                             
@@ -87,7 +91,7 @@ const HomePage = function (){
                                     />
                                 </div>
                                 <div className="py-2">
-                                        <h5 className="mb-2 text-xl font-bold tracking-tight text-gray-900 dark:text-black text-center">Piscina</h5>
+                                        <h5 className="mb-2 text-xl font-bold tracking-tight text-black text-center">Piscina</h5>
                                 </div>
                             </div>
 
@@ -101,7 +105,7 @@ const HomePage = function (){
                                     />
                                 </div>
                                 <div className="py-2">
-                                        <h5 className="mb-2 text-xl font-bold tracking-tight text-gray-900 dark:text-black text-center">Relax</h5>
+                                        <h5 className="mb-2 text-xl font-bold tracking-tight text-black text-center">Relax</h5>
                                 </div>
                             </div>
                             <div className="w-full mx-auto">
@@ -113,7 +117,7 @@ const HomePage = function (){
                                     />
                                 </div>
                                 <div className="py-2">
-                                        <h5 className="mb-2 text-xl font-bold tracking-tight text-gray-900 dark:text-black text-center">Ristorante</h5>
+                                        <h5 className="mb-2 text-xl font-bold tracking-tight text-black text-center">Ristorante</h5>
                                 </div>
                             </div>
                         </div>
@@ -128,13 +132,13 @@ const HomePage = function (){
                                     <div className="flex-1">
                                         <img src="https://images.pexels.com/photos/4989099/pexels-photo-4989099.jpeg" alt="" className="aspect-3/2 object-cover object-[25%_70%] rounded-2xl shadow-lg/30" />
                                         <div className="p-5">
-                                            <h5 className="mb-2 text-xl font-bold tracking-tight text-gray-900 dark:text-black text-center">Bagni e docce calde</h5>
+                                            <h5 className="mb-2 text-xl font-bold tracking-tight text-black text-center">Bagni e docce calde</h5>
                                         </div>
                                     </div>
                                     <div className="flex-1">
                                         <img src="https://images.pexels.com/photos/67184/pexels-photo-67184.jpeg" alt="" className="aspect-3/2 object-cover object-bottom rounded-2xl shadow-lg/30" />
                                         <div className="p-5">
-                                            <h5 className="mb-2 text-xl font-bold tracking-tight text-gray-900 dark:text-black text-center">Lavandini all'aperto</h5>
+                                            <h5 className="mb-2 text-xl font-bold tracking-tight text-black text-center">Lavandini all'aperto</h5>
                                         </div>
                                     </div>
                                 </div>
@@ -143,13 +147,13 @@ const HomePage = function (){
                                     <div className="flex-1">
                                         <img src="https://images.pexels.com/photos/1309067/pexels-photo-1309067.jpeg" alt="" className="aspect-3/2 object-cover object-bottom rounded-2xl shadow-lg/30" />
                                         <div className="p-5">
-                                            <h5 className="mb-2 text-xl font-bold tracking-tight text-gray-900 dark:text-black text-center">Barbecue</h5>
+                                            <h5 className="mb-2 text-xl font-bold tracking-tight text-black text-center">Barbecue</h5>
                                         </div>
                                     </div>
                                     <div className="flex-1">
                                         <img src="https://images.pexels.com/photos/1061640/pexels-photo-1061640.jpeg" alt="" className="aspect-3/2 object-cover object-bottom rounded-2xl shadow-lg/30" />
                                         <div className="p-5">
-                                            <h5 className="mb-2 text-xl font-bold tracking-tight text-gray-900 dark:text-black text-center">Falò</h5>
+                                            <h5 className="mb-2 text-xl font-bold tracking-tight text-black text-center">Falò</h5>
                                         </div>
                                     </div>
                                 </div>
@@ -161,15 +165,15 @@ const HomePage = function (){
                         {/* dintorni */}
                         <div className="flex flex-col justify-between items-center gap-8 center">
                             <h3 className="text-3xl quicksand-titles font-medium text-shadow-md/20">Dintorni</h3>
-                            <p>Visita la toscana! da spiagge caraibiche a discoteche a picco sul mare</p>
+                            <p className="text-lg">Visita la toscana! da spiagge caraibiche a discoteche a picco sul mare.</p>
                             <div className="flex flex-col sm:flex-row gap-8">
                                 <div className="flex-1">
                                     <Link to={'/surroundings'}>
                                         <img src="https://images.pexels.com/photos/2049422/pexels-photo-2049422.jpeg" alt="" className="aspect-3/2 object-cover object-bottom rounded-2xl shadow-lg/30 transition delay-50 duration-300 ease-in-out hover:-translate-y-1 hover:scale-110" />
                                     </Link>
                                         <div className="p-2">
-                                            <h5 className="mb-2 text-xl font-bold tracking-tight text-gray-900 dark:text-black text-center">Mare</h5>
-                                            <p className="text-center">descrizione spiagge etc?</p>
+                                            <h5 className="mb-2 text-xl font-bold tracking-tight text-black text-center">Mare</h5>
+                                            <p className="text-center">Troverai di tutto di più: spiagge bianche, scogli colorati e resort con piscine sul mare</p>
                                         </div>
                                 </div>
                                 <div className="flex-1">
@@ -177,8 +181,8 @@ const HomePage = function (){
                                         <img src="https://images.pexels.com/photos/2629233/pexels-photo-2629233.jpeg" alt="" className="aspect-3/2 object-cover object-center rounded-2xl shadow-lg/30 transition delay-50 duration-300 ease-in-out hover:-translate-y-1 hover:scale-110" />
                                     </Link>
                                         <div className="p-2">
-                                            <h5 className="mb-2 text-xl font-bold tracking-tight text-gray-900 dark:text-black text-center">Trekking</h5>
-                                            <p className="text-center">descrizione natura etc?</p>
+                                            <h5 className="mb-2 text-xl font-bold tracking-tight text-black text-center">Trekking</h5>
+                                            <p className="text-center">Percorri una delle tante antiche strade romane immerse nel verde</p>
                                         </div>
                                 </div>
                                 <div className="flex-1">
@@ -186,8 +190,8 @@ const HomePage = function (){
                                         <img src="https://images.pexels.com/photos/4430307/pexels-photo-4430307.jpeg" alt="" className="aspect-3/2 object-cover object-center rounded-2xl shadow-lg/30 transition delay-50 duration-300 ease-in-out hover:-translate-y-1 hover:scale-110" />
                                     </Link>
                                         <div className="p-2">
-                                            <h5 className="mb-2 text-xl font-bold tracking-tight text-gray-900 dark:text-black text-center">Vita notturna</h5>
-                                            <p className="text-center">descrizione discoteche ristoranti etc?</p>
+                                            <h5 className="mb-2 text-xl font-bold tracking-tight text-black text-center">Vita notturna</h5>
+                                            <p className="text-center">Da ristoranti chic in giacca e cravatta ad aperitivi scalzi in spiaggia</p>
                                         </div>
                                 </div>
                             </div>
@@ -200,16 +204,15 @@ const HomePage = function (){
 
             </div>
             {/* find us */}
-                        <div className="flex flex-col bg-gray-300 items-center gap-8 p-6">
+                        <div className="flex flex-col bg-gray-300 items-center gap-8 p-6 text-black">
                             <h3 className="text-3xl text-shadow-md/20">Dove siamo</h3>
                             
                             <div className="flex flex-col md:flex-row w-full gap-20">
                                 <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d5790.225290108554!2d10.32744894740432!3d43.47911951957426!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x12d5e61e2519c089%3A0x70bfd41bcbc381dd!2sCalafuria%20Province%20of%20Livorno%2C%20Italy!5e0!3m2!1sen!2ses!4v1752496589592!5m2!1sen!2ses" width="600" height="450" loading="lazy" className="w-full flex-2"></iframe>
                                 <div className="flex flex-col items-center justify-center flex-1 w-full text-center">
-                                    <p>descrizione strada</p>
-                                    <p>Via aereo:</p>
+                                    <p>Aeroporto più vicino:</p>
                                     <p>Aeroporto di pisa</p>
-                                    <p>Autostrada</p>
+                                    <p>Uscita Autostrada Genova - Grosseto: 92</p>
                                 </div>
                             </div>
 
